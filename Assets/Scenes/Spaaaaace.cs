67887f33e87a14da1f7aa7e0781aadf6def599bd
Doug Lubaway <douglaslubaway@gmail.com>
2019-09-15 21:56:58 -0400

    Spaceship Creation

---
+++ Assets/Scenes/Spaaaaace.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaaaaace : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("IMACOMPUTA");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            print("IMALEFTCOMPUTA");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("IMARIGHTCOMPUTA");
+        }
+    }
+}
